e51c0a527c5ed1b247aca1b26d3d715324914b3e
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2021-05-17 09:05:33 +0700

    add proxy to query real data

--- craco.config.js
+++ craco.config.js
@@ -3,5 +3,10 @@ module.exports = {
     postcss: {
       plugins: [require('tailwindcss'), require('autoprefixer')]
     }
+  },
+  devServer: {
+    proxy: {
+      '/v_bl/109': 'http://negaraapi.madiunkota.go.id:8888'
+    }
   }
 };
